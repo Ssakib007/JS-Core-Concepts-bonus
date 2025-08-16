9b497d0790c074985b74d05b225e1d4438748f2b
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-16 21:56:53 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+  name: "ron",
+  id: 121,
+  address: "movie cinema",
+  isSingle: true,
+  friends: ["apu", "nap", "sal", "ami"],
+  car: {
+    brand: "tesla",
+    price: 5000000,
+    made: 2025,
+  },
+};
+console.log(student);
