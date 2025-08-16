974c9d76229d40e3731dd5ed2ef468d46111303d
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-17 00:14:50 +0600

    arguments and array like object

--- object.js
+++ object.js
@@ -1,13 +1,29 @@
-const student = {
+const hero = {
   name: "ron",
   id: 121,
   address: "movie cinema",
   isSingle: true,
   friends: ["apu", "nap", "sal", "ami"],
+  movies: [
+    { name: "no 1", year: 2015 },
+    { name: "king khan", year: 2018 },
+  ],
+  act: function () {
+    console.log("active like ron");
+  },
   car: {
     brand: "tesla",
     price: 5000000,
     made: 2025,
+    manufacturer: {
+      name: "tesla",
+      ceo: "mask",
+      country: "usa",
+    },
   },
 };
-console.log(student);
+console.log(hero.act);
+hero.act();
+
+
+
